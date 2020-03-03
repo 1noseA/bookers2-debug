de8220cfcad9d080f8b84d95e4487c1ac35e847c
1noseA <1noseA2L@gmail.com>
2020-03-03 14:30:31 +0900

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-	before_action :baria_user, only: [:update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
   	@user = User.find(params[:id])
@@ -20,9 +20,9 @@ class UsersController < ApplicationController
   def update
   	@user = User.find(params[:id])
   	if @user.update(user_params)
-  		redirect_to users_path(@user), notice: "successfully updated user!"
+  		redirect_to user_path(@user), notice: "successfully updated user!"
   	else
-  		render "show"
+  		render "edit"
   	end
   end
 
@@ -32,10 +32,11 @@ class UsersController < ApplicationController
   end
 
   #url直接防止　メソッドを自己定義してbefore_actionで発動。
-   def baria_user
-  	unless params[:id].to_i == current_user.id
-  		redirect_to user_path(current_user)
+  def correct_user
+    user = User.find(params[:id])
+    if current_user != user
+      redirect_to user_path(current_user)
     end
-   end
+  end
 
 end
